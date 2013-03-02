520654be3146aea775e2dba884b7b22c9e77aff4
Igor Soarez <igorsoarez@gmail.com>
2013-03-02 06:55:32 +0000

    Create comunication system

---
+++ server.js
@@ -0,0 +1,49 @@
+
+var http = require('http');
+
+var fs = require('fs');
+var path = require('path');
+var controllerPath = 'controller.html';
+var playerPath = 'player.html';
+
+var players = [];
+
+function requestListener(req, res){
+  console.log(req.url);
+  if (~req.url.indexOf('player'))
+    fs.createReadStream(path.join(__dirname, playerPath)).pipe(res);
+  else
+    fs.createReadStream(path.join(__dirname, controllerPath)).pipe(res);
+}
+
+function onListening() {
+  console.log('Server started on port %d', port);
+}
+
+function onWsConnection(ws) {
+  if (~ws.upgradeReq.url.indexOf('player')) {
+    players.push(ws);
+    ws.on('close', onPlayerClose.bind(ws));
+  } else
+    ws.on('message', onControllerMessage);
+}
+
+function onPlayerClose() {
+  players.splice(players.indexOf(this), 1);
+}
+
+function onControllerMessage(message) {
+  console.log('Msg from controller: "%s"', message);
+
+  var i;
+  for (i=0; i<players.length; ++i)
+    players[i].send(message);
+}
+
+var server = http.createServer(requestListener);
+var port = parseInt(process.env.PORT) || 8000;
+server.listen(port, onListening);
+
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({ server: server });
+wss.on('connection', onWsConnection);
